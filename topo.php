f775f6a517f93a07d7a8d4be45f5fc0acb30b351
Luana Reis <45272947+luanasr@users.noreply.github.com>
2019-10-24 17:29:36 -0300

    Add files via upload

---
+++ topo.php
@@ -0,0 +1,76 @@
+<html>
+	<head>
+		<title>Twitter: É o que está acontecendo</title>
+		<meta charset="UTF-8">
+		<link rel="shortcut icon"  a href="imagens/fav.png" />
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css"><link rel="shortcut icon"  a href="imagens/fav.png" />
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" >
+		
+	<style>
+		body{
+			 background-image: url("imagens/inicial.png");	
+			 background-size: cover;
+			 background-repeat: no-repeat;
+			
+			}
+		h1{
+			align: center;
+			valign: middle;
+		}
+#inicio{
+	position: absolute;
+	background-color: white;	
+    width: 400px;
+    height: 160px;
+    margin-top: -50px;
+    margin-left: -150px;
+    top: 20%;
+    left: 72%;
+	text-align:center;
+	}
+	#login{
+	position: absolute;
+	background-color: white;	
+    width: 400px;
+    height: 160px;
+    margin-top: -50px;
+    margin-left: -150px;
+    top: 10%;
+    left: 72%;
+	text-align:center;
+	}
+ 
+	#perfil{
+	position: absolute;
+	background-color: white;
+    width: 400px;
+    height: 150px;
+    margin-top: -150px;
+    margin-left: -150px;
+    top: 30%;
+    left: 70%;
+	text-align:center;
+	}
+#amigos{
+	position: absolute;
+     margin-top: -150px;
+    margin-left: -150px;
+	top:30%;
+    left: 40%;
+	text-align:center;
+}
+#busca{
+	position: absolute;
+     margin-top: -150px;
+    margin-left: -150px;
+	top:45%;
+    left: 40%;
+	text-align:center;
+}
+*/
+	</style>
+	</head>
+	<body>
